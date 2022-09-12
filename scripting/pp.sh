e547025045aa98b0a4bd00ba8797eaef22138723
root <root@ip-172-31-88-95.ec2.internal>
2022-09-12 10:25:33 +0000

    added variable script

---
+++ scripting/pp.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "enter your friends names:"
+read CPS
+echo "my friends $CPS date of births is"
+echo "my file name is $0"
+echo "chimpu born in the month of $1"
+echo "prathima born in the month of $2"
+echo "sotta born in the month of $3"
+
+echo "tenth parameter is ${10}"
+
+echo "how many parameters used in the script $@"
+echo "total parameters count $#"
